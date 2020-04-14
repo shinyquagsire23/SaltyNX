3fcd545730298580a00209ee83ee45b6c799cd6e
masagrator <masagrator@gmail.com>
2020-04-14 13:32:08 +0200

    Port spawner to libnx 3.1.0 (tests deleted)

--- saltysd_spawner/source/elf_parser.hpp
+++ saltysd_spawner/source/elf_parser.hpp
@@ -55,8 +55,6 @@ class Elf_parser {
              
         Elf_parser(char* path)
         {
-            Elf64_Shdr sh_strtab;
-            //char* sh_strtab_p;
             
             load_error = 0;
             FILE* f = NULL;
@@ -70,20 +68,6 @@ class Elf_parser {
             }
             
             fread(&ehdr, sizeof(ehdr), 1, f);
-            /*{
-                fclose(f);
-                
-                load_error = 2;
-                return;
-            }*/
-            
-            // Read symtab
-            /*fseek(f, ehdr.e_shoff + ehdr.e_shstrndx * sizeof(Elf64_Shdr), SEEK_SET);
-            fread(&sh_strtab, sizeof(sh_strtab), 1, f);
-            
-            sh_strtab_p = (char*)malloc(sh_strtab.sh_size);
-            fseek(f, sh_strtab.sh_offset, SEEK_SET);
-            fread(&sh_strtab_p, sh_strtab.sh_size, 1, f);*/
 
             // Read sections
             for (int i = 0; i < ehdr.e_shnum; ++i) {
@@ -93,7 +77,6 @@ class Elf_parser {
             
                 section_t section;
                 section.section_index= i;
-                //section.section_name = std::string(sh_strtab_p + read.sh_name);
                 section.section_type = get_section_type(read.sh_type);
                 section.section_addr = read.sh_addr;
                 section.section_offset = read.sh_offset;
--- saltysd_spawner/source/ipc_handoffs.c
+++ saltysd_spawner/source/ipc_handoffs.c
@@ -1,211 +1,29 @@
 #include "ipc_handoffs.h"
 
-#include <switch_min/kernel/ipc.h>
+#include <switch.h>
 
 #include "useful.h"
 
 bool should_terminate = false;
 
-Result fsp_init(Service fsp)
+Result fsp_init()
 {
     Result rc;
-    IpcCommand c;
-    ipcInitialize(&c);
-    ipcSendPid(&c);
-
-    struct {
-        u64 magic;
-        u64 cmd_id;
-        u64 unk;
-    } *raw;
-
-    raw = ipcPrepareHeader(&c, sizeof(*raw));
-
-    raw->magic = SFCI_MAGIC;
-    raw->cmd_id = 1;
-    raw->unk = 0;
-
-    rc = serviceIpcDispatch(&fsp);
-
-    if (R_SUCCEEDED(rc)) {
-        IpcParsedCommand r;
-        ipcParse(&r);
-
-        struct {
-            u64 magic;
-            u64 result;
-        } *resp = r.Raw;
-
-        rc = resp->result;
-    }
-    
-    return rc;
+    rc = fsInitialize();
+	return rc;
 }
 
-Result fsp_getSdCard(Service fsp, Handle* out)
+Result fsp_getSdCard()
 {
     Result rc;
-    IpcCommand c;
-    ipcInitialize(&c);
-
-    struct {
-        u64 magic;
-        u64 cmd_id;
-    } *raw;
-
-    raw = ipcPrepareHeader(&c, sizeof(*raw));
-
-    raw->magic = SFCI_MAGIC;
-    raw->cmd_id = 18;
-
-    rc = serviceIpcDispatch(&fsp);
-
-    if (R_SUCCEEDED(rc)) {
-        IpcParsedCommand r;
-        ipcParse(&r);
-
-        struct {
-            u64 magic;
-            u64 result;
-        } *resp = r.Raw;
-
-        *out = r.Handles[0];
-
-        rc = resp->result;
-    }
-    
-    return rc;
-}
-
-Result handle_cmd(int cmd)
-{
-    Result ret = 0;
-
-    // Send reply
-    IpcCommand c;
-    ipcInitialize(&c);
-    ipcSendPid(&c);
-
-    if (cmd == 1)
-    {
-        IpcParsedCommand r;
-        ipcParse(&r);
-
-        struct {
-            u64 magic;
-            u64 command;
-            char name[12];
-            u32 reserved;
-        } *resp = r.Raw;
-        
-        char name[12];
-        u64 pid = r.Pid;
-        memcpy(name, resp->name, 12);
-
-        SaltySD_printf("Spawner: SaltySD (pid %x) asked for handle %s\n", pid, name);
-
-        if (!strcmp(name, "sdcard"))
-        {
-            Service toget;
-            Handle sdcard;
-            ret = smGetService(&toget, "fsp-srv");
-            if (ret)
-                SaltySD_printf("Spawner: couldn't get fsp-srv handle, ret %x\n", ret);
-            
-            if (!ret)
-            {
-                ret = fsp_init(toget);
-                if (ret)
-                    SaltySD_printf("Spawner: couldn't init fsp, ret %x\n", ret);
-            }
-
-            if (!ret)
-                ret = fsp_getSdCard(toget, &sdcard);
-            
-            if (!ret)
-                ipcSendHandleMove(&c, sdcard);
-            else
-                SaltySD_printf("Spawner: couldn't get SdCard handle, ret %x\n", ret);
-        }
-        else
-        {
-            Service toget;
-            ret = smGetService(&toget, name);
-            if (!ret && !strcmp(name, "fsp-srv"))
-            {
-                ret = fsp_init(toget);
-            }
-
-            if (!ret)
-                ipcSendHandleMove(&c, toget.handle);
-            else
-                SaltySD_printf("Spawner: couldn't get handle, ret %x\n", ret);
-        }
-    }
-    else if (cmd == 2)
-    {
-        should_terminate = true;
-        ret = 0;
-    }
-    else
-    {
-        ret = 0xEE01;
-    }
-    
-    struct {
-        u64 magic;
-        u64 result;
-        u64 reserved[2];
-    } *raw;
-
-    raw = ipcPrepareHeader(&c, sizeof(*raw));
-
-    raw->magic = SFCO_MAGIC;
-    raw->result = ret;
-}
-
-void saltysd_test(Handle port)
-{
-    Result ret;
-
-    // Send a command
-    IpcCommand c;
-    ipcInitialize(&c);
-    ipcSendPid(&c);
-
-    struct {
-        u64 magic;
-        u64 cmd_id;
-        u64 reserved[2];
-    } *raw;
-
-    raw = ipcPrepareHeader(&c, sizeof(*raw));
-
-    raw->magic = SFCI_MAGIC;
-    raw->cmd_id = 0;
-
-    SaltySD_printf("Spawner: sending SaltySD test\n");
-    ret = ipcDispatch(port);
-
-    if (R_SUCCEEDED(ret)) {
-        IpcParsedCommand r;
-        ipcParse(&r);
-
-        struct {
-            u64 magic;
-            u64 result;
-            u64 reserved[2];
-        } *resp = r.Raw;
-
-        ret = resp->result;
-    }
+    rc = fsdevMountSdmc();
+	return rc;
 }
 
 Result ipc_handoffs()
 {
     Result ret;
     Handle port;
-    Service fsp;
     
     ret = svcManageNamedPort(&port, "Spawner", 1);
     
@@ -215,55 +33,34 @@ Result ipc_handoffs()
         ret = svcAcceptSession(&session, port);
         if (ret && ret != 0xf201)
         {
-            //SaltySD_printf("Spawner: svcAcceptSession returned %x\n", ret);
+            SaltySD_printf("Spawner: svcAcceptSession returned %x\n", ret);
         }
         else if (!ret)
         {
             int handle_index;
-            int reply_num = 0;
             Handle replySession = 0;
             while (1)
             {
-                ret = svcReplyAndReceive(&handle_index, &session, 1, replySession, U64_MAX);
+                ret = svcReplyAndReceive(&handle_index, &session, 1, replySession, UINT64_MAX);
                 
-                if (should_terminate) break;
-                
-                if (ret) break;
-                
-                IpcParsedCommand r;
-                ipcParse(&r);
-
-                struct {
-                    u64 magic;
-                    u64 command;
-                    u64 reserved[2];
-                } *resp = r.Raw;
-
-                handle_cmd(resp->command);
-
-                replySession = session;
-                svcSleepThread(1000*1000);
+                if (!ret) break;
             }
             
             svcCloseHandle(session);
         }
-
-        if (should_terminate) break;
         svcSleepThread(1000*1000);
     }
     
     svcCloseHandle(port);
     
     // Test SaltySD commands
-    Service saltysd;
+    Handle saltysd;
     do
     {
         ret = svcConnectToNamedPort(&saltysd, "SaltySD");
         svcSleepThread(1000*1000);
     }
     while (ret);
-    saltysd_test(saltysd.handle);
-    svcCloseHandle(saltysd.handle);
 
     return ret;
 }
